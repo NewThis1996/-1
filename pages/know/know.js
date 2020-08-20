967d92f386feacf7f6fcd9b137a48ea03dbef730
NewThis1996 <702381450@qq.com>
2020-08-20 23:38:51 +0800

    添加项目

---
+++ pages/know/know.js
@@ -0,0 +1,77 @@
+// pages/know/know.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name:"小明",
+    // 要渲染的话,还是用{{}}表示
+
+
+    info:"<text>188</text>",
+    // 要是渲染标签的话,就需要用
+    // <rich-text node="{{info}}"></rich-text>
+
+
+
+    // 列表渲染
+    arrs:['苹果','香蕉','橘子']
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
